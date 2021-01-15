7a7f65fb20e641fdebcd4a73290b8155703f5443
ConnorMul <mulhollandconnor@icloud.com>
2021-01-15 09:08:57 -0500

    commit tree b93d94364c53fd664ce200c888f29e0ac13b1e63

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
@@ -6,6 +6,41 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  // useEffect(() => {
+  //   fetch("http://localhost:3001/pets")
+  //   .then(resp => resp.json())
+  //   .then(petsObjs => setPets(petsObjs))
+  // }, [])
+  
+  const filteredPets = pets.filter((pet) => 
+    filters.type === "all" || pet.type === filters.type
+  )
+
+  function handleFindPetsClick() {
+    if (filters.type === "all") {
+      fetch("http://localhost:3001/pets")
+      .then(resp => resp.json())
+      .then(petsObjs => setPets(petsObjs))
+    } else if (filters.type === "cat") {
+      fetch("http://localhost:3001/pets?type=cat")
+      .then(resp => resp.json())
+      .then(petsObjs => setPets(petsObjs))
+    } else if (filters.type === "dog") {
+      fetch("http://localhost:3001/pets?type=dog")
+      .then(resp => resp.json())
+      .then(petsObjs => setPets(petsObjs))
+    } else {
+      fetch("http://localhost:3001/pets?type=micropig")
+      .then(resp => resp.json())
+      .then(petsObjs => setPets(petsObjs))
+    }
+  }
+
+  function handleAdoptPet(id) {
+    const updatedPets = filteredPets.map((pet) => pet.id === id ? {...pet, isAdopted: true} : pet)
+    setPets(updatedPets)
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +50,17 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters 
+              filters={filters}
+              onChangeType={setFilters}
+              onFindPetsClick={handleFindPetsClick}
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser 
+              pets={filteredPets}
+              onAdoptPet={handleAdoptPet}
+            />
           </div>
         </div>
       </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,9 +1,29 @@
 import React from "react";
-
 import Pet from "./Pet";
 
-function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+function PetBrowser({ pets, onAdoptPet }) {
+  
+  const petComps = pets.map((pet) => {
+    return (
+    <Pet
+      key={pet.id} 
+      id={pet.id}
+      type={pet.type}
+      gender={pet.gender}
+      age={pet.age}
+      weight={pet.weight}
+      name={pet.name}
+      isAdopted={pet.isAdopted}
+      onAdoptPet={onAdoptPet}
+    />
+    )
+  })
+  
+  return (
+      <div className="ui cards">
+        {petComps}
+      </div>
+  )
 }
 
 export default PetBrowser;
